e484e920d43cebdb1829b3f2227a41e1a85bb5a2
Kevin Ballard <kevin@sb.org>
2014-08-10 20:36:29 -0700

    Implement reverse federation

--- node/stellarFederation.js
+++ node/stellarFederation.js
@@ -30,30 +30,43 @@ var STELLAR_USERS = (function() {
 // Include dependencies
 var express = require('express');
 var morgan = require('morgan');
+var url = require('url');
 
 // Define morgan logging tokens
 morgan.token('federation-type', function(req, res) {
   return res.locals.federationType;
 });
-morgan.token('federation-destination', function(req, res) {
-  var dest = res.locals.federationDestination || "";
-  var domain = res.locals.federationDomain || "";
-  if (dest || domain) {
-    return '"' + dest + '@' + domain + '"';
+morgan.token('federation-request', function(req, res) {
+  if (res.locals.federationType === 'federation') {
+    var dest = res.locals.federationDestination || "";
+    var domain = res.locals.federationDomain || "";
+    if (dest || domain) {
+      return '"' + dest + '@' + domain + '"';
+    }
+  } else {
+    return res.locals.federationDestinationAddress;
   }
 });
 morgan.token('federation-result', function(req, res) {
   return res.locals.federationResult;
 });
+morgan.token('url-path', function(req, res) {
+  return url.parse(req.originalUrl || req.url).pathname;
+});
 
 // Create the server
 var app = express();
 app.enable('trust proxy');
-app.use(morgan(':remote-addr [:date] ":method :url HTTP/:http-version" :federation-type :federation-destination :status :federation-result', {
+app.use(morgan(':remote-addr [:date] ":method :url-path HTTP/:http-version" :federation-type :federation-request :status :federation-result', {
   stream: process.stdout
 }));
 
-// Define error handling
+// Utility functions
+function getQueryString(req, name) {
+  var val = req.query[name];
+  return typeof val === 'string' ? val : null;
+}
+
 function sendError(res, req, error) {
   var ERROR_MESSAGES = {
     noSuchDomain: 'Invalid Domain',
@@ -68,10 +81,15 @@ function sendError(res, req, error) {
   });
 }
 
-// Utility functions
-function getQueryString(req, name) {
-  var val = req.query[name];
-  return typeof val === 'string' ? val : null;
+function sendFederationJson(req, res) {
+  res.send({
+    federation_json: {
+      type:                'federation_record',
+      destination:         res.locals.federationDestination,
+      domain:              res.locals.federationDomain,
+      destination_address: res.locals.federationDestinationAddress
+    }
+  });
 }
 
 // Accept federation requests
@@ -89,15 +107,29 @@ app.get('/federation', function(req, res) {
   } else if (!address) {
     sendError(res, req, 'noSuchUser');
   } else {
-    res.locals.federationResult = address;
-    res.send({
-      federation_json: {
-        type:                'federation_record',
-        destination:         res.locals.federationDestination,
-        domain:              res.locals.federationDomain,
-        destination_address: res.locals.federationResult
-      }
-    });
+    res.locals.federationResult = res.locals.federationDestinationAddress = address;
+    sendFederationJson(req, res);
+  }
+});
+
+// Accept reverseFederation requests
+app.get('/reverseFederation', function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  // ignore the type=reverse_federation key, as apparently the stellar.org federation server does
+  res.locals.federationType = 'reverse_federation';
+  res.locals.federationDestinationAddress = getQueryString(req, 'destination_address');
+  res.locals.federationDomain = getQueryString(req, 'domain');
+
+  var addresses = STELLAR_ADDRESSES[res.locals.federationDomain];
+  var users = addresses && addresses[res.locals.federationDestinationAddress];
+  if (!addresses) {
+    sendError(res, req, 'noSuchDomain');
+  } else if (!users || !(typeof users !== 'string' && users[0])) {
+    sendError(res, req, 'noSuchUser');
+  } else {
+    var user = typeof users === 'string' ? users : users[0];
+    res.locals.federationResult = res.locals.federationDestination = user;
+    sendFederationJson(req, res);
   }
 });
 

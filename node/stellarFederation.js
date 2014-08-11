0e6be5491244e9c66825bbc38511bc65ab9a34f0
Kevin Ballard <kevin@sb.org>
2014-08-10 19:28:47 -0700

    Add logging using morgan

--- node/stellarFederation.js
+++ node/stellarFederation.js
@@ -8,10 +8,29 @@ var STELLAR_ADDRESSES = {
 
 // Include dependencies
 var express = require('express');
+var morgan = require('morgan');
+
+// Define morgan logging tokens
+morgan.token('federation-type', function(req, res) {
+  return res.locals.federationType;
+});
+morgan.token('federation-destination', function(req, res) {
+  var dest = res.locals.federationDestination || "";
+  var domain = res.locals.federationDomain || "";
+  if (dest || domain) {
+    return '"' + dest + '@' + domain + '"';
+  }
+});
+morgan.token('federation-result', function(req, res) {
+  return res.locals.federationResult;
+});
 
 // Create the server
 var app = express();
 app.enable('trust proxy');
+app.use(morgan(':remote-addr [:date] ":method :url HTTP/:http-version" :federation-type :federation-destination :status :federation-result', {
+  stream: process.stdout
+}));
 
 // Define error handling
 function sendError(res, req, error) {
@@ -19,6 +38,7 @@ function sendError(res, req, error) {
     noSuchDomain: 'Invalid Domain',
     noSuchUser: 'No user found'
   };
+  res.locals.federationResult = error;
   res.send(400, {
     result:        'error',
     error:         error,
@@ -27,21 +47,32 @@ function sendError(res, req, error) {
   });
 }
 
+// Utility functions
+function getQueryString(req, name) {
+  var val = req.query[name];
+  return typeof val === 'string' ? val : null;
+}
+
 // Accept federation requests
 app.get('/federation', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   // ignore the type=federation key, as apparently the stellar.org federation server does
-  if (req.query.domain !== DOMAIN_NAME) {
+  res.locals.federationType = 'federation';
+  res.locals.federationDestination = getQueryString(req, 'destination');
+  res.locals.federationDomain = getQueryString(req, 'domain');
+
+  if (res.locals.federationDomain !== DOMAIN_NAME) {
     sendError(res, req, 'noSuchDomain');
-  } else if (!STELLAR_ADDRESSES[req.query.destination]) {
+  } else if (!STELLAR_ADDRESSES[res.locals.federationDestination]) {
     sendError(res, req, 'noSuchUser');
   } else {
+    res.locals.federationResult = STELLAR_ADDRESSES[res.locals.federationDestination];
     res.send({
       federation_json: {
         type:                'federation_record',
-        destination:         req.query.destination,
+        destination:         res.locals.federationDestination,
         domain:              DOMAIN_NAME,
-        destination_address: STELLAR_ADDRESSES[req.query.destination]
+        destination_address: res.locals.federationResult
       }
     });
   }
